b3091bb300d87cec74a0d0bd7af18adbbe3d19f4
Charles Ji <me@charlesji.com>
2023-08-25 15:31:38 -0400

    Add search and filtering to browse page

--- app/browse/page.tsx
+++ app/browse/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState, useMemo } from 'react';
 import { Header } from '@/components/header';
 import { ReviewCard } from '@/components/review-card';
 import { useReviews } from '@/hooks/use-reviews';
@@ -7,6 +8,51 @@ import Link from 'next/link';
 
 export default function BrowsePage() {
   const { data: reviews, isLoading, error } = useReviews();
+  const [searchQuery, setSearchQuery] = useState('');
+  const [sortBy, setSortBy] = useState<'newest' | 'highest' | 'most-voted'>('newest');
+
+  // Filter and sort reviews
+  const filteredReviews = useMemo(() => {
+    if (!reviews) return [];
+
+    let filtered = reviews;
+
+    // Filter by search query (package ID)
+    if (searchQuery) {
+      filtered = filtered.filter((review) =>
+        review.targetPackage.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+    }
+
+    // Sort reviews
+    const sorted = [...filtered].sort((a, b) => {
+      switch (sortBy) {
+        case 'highest':
+          const scoreA =
+            a.ratingSecurity * 0.3 +
+            a.ratingUsability * 0.25 +
+            a.ratingPerformance * 0.2 +
+            a.ratingDocumentation * 0.15 +
+            a.ratingInnovation * 0.1;
+          const scoreB =
+            b.ratingSecurity * 0.3 +
+            b.ratingUsability * 0.25 +
+            b.ratingPerformance * 0.2 +
+            b.ratingDocumentation * 0.15 +
+            b.ratingInnovation * 0.1;
+          return scoreB - scoreA;
+        case 'most-voted':
+          const votesA = parseInt(a.upvotes) + parseInt(a.downvotes);
+          const votesB = parseInt(b.upvotes) + parseInt(b.downvotes);
+          return votesB - votesA;
+        case 'newest':
+        default:
+          return parseInt(b.timestamp) - parseInt(a.timestamp);
+      }
+    });
+
+    return sorted;
+  }, [reviews, searchQuery, sortBy]);
 
   return (
     <div className="min-h-screen bg-gray-50">
@@ -18,7 +64,7 @@ export default function BrowsePage() {
               Browse Reviews
             </h1>
             <p className="text-gray-600">
-              Discover what the community thinks about Sui dApps
+              {filteredReviews.length} {filteredReviews.length === 1 ? 'review' : 'reviews'} found
             </p>
           </div>
           <Link
@@ -27,6 +73,32 @@ export default function BrowsePage() {
           >
             Submit Review
           </Link>
+        </div>
+
+        {/* Search and Filter Controls */}
+        <div className="bg-white rounded-lg border border-gray-200 p-4 mb-6">
+          <div className="flex flex-col md:flex-row gap-4">
+            <div className="flex-1">
+              <input
+                type="text"
+                placeholder="Search by package ID..."
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              />
+            </div>
+            <div className="flex gap-2">
+              <select
+                value={sortBy}
+                onChange={(e) => setSortBy(e.target.value as any)}
+                className="px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent bg-white"
+              >
+                <option value="newest">Newest First</option>
+                <option value="highest">Highest Rated</option>
+                <option value="most-voted">Most Voted</option>
+              </select>
+            </div>
+          </div>
         </div>
 
         {isLoading && (
@@ -56,9 +128,23 @@ export default function BrowsePage() {
           </div>
         )}
 
-        {!isLoading && !error && reviews && reviews.length > 0 && (
+        {!isLoading && !error && filteredReviews.length === 0 && reviews && reviews.length > 0 && (
+          <div className="bg-white rounded-lg border border-gray-200 p-12 text-center">
+            <p className="text-gray-600 mb-4">
+              No reviews match your search.
+            </p>
+            <button
+              onClick={() => setSearchQuery('')}
+              className="text-blue-600 hover:text-blue-700 font-medium"
+            >
+              Clear search
+            </button>
+          </div>
+        )}
+
+        {!isLoading && !error && filteredReviews.length > 0 && (
           <div className="space-y-6">
-            {reviews.map((review) => (
+            {filteredReviews.map((review) => (
               <ReviewCard key={review.id} review={review} />
             ))}
           </div>
